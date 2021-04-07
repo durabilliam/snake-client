2e40238298dcd3ca41a8258afc3caa132996e3b3
William Perry <durabilliam@gamil.com>
2021-04-07 09:22:41 -0230

    used hard coded commands to move and tested setTimout and setInterval to make snake look animated

--- client.js
+++ client.js
@@ -13,8 +13,35 @@ const connect = function() {
 
   conn.on('connect', () => {
     conn.write('Name: BBP');
-    console.log('Successfully connected to game server')
-  })
+    console.log('Successfully connected to game server');
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // conn.write("Move: up");
+    // setTimeout(() => {
+    //   conn.write("Move: up");;
+    // }, 1000);
+    // setTimeout(() => {
+    //   conn.write("Move: up");;
+    // }, 1500);
+    // setTimeout(() => {
+    //   conn.write("Move: up");;
+    // }, 2000);
+    // setTimeout(() => {
+    //   conn.write("Move: up");;
+    // }, 2500);
+
+    // setInterval(() => {
+    //    conn.write("Move: up");;
+    //  }, 1000);
+
+    // setInterval(() => {
+    //   conn.write("Move: Left");;
+    // }, 1000);
+  
+    })
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
